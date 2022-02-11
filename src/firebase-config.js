bbe92984c78782174f28d857d8b28bf9f1dc28f0
RainersLeitarts <rainers.leitarts4@gmail.com>
2022-02-11 22:56:27 +0200

    cleanup

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,10 +10,6 @@ const firebaseConfig = {
   appId: "1:599629056903:web:684ccddb8e081bca336122"
 };
 
-
-
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore()
